c815786b56112c6c09b7153880944a47bcc039ac
tiiimm <51147200+tiiimm@users.noreply.github.com>
2019-10-31 14:59:09 +0800

    fixed set_user_type api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,7 @@ use App\Philanthropist;
 use App\PhilanthropistPoint;
 use App\Role;
 use App\User;
-use ErrorException;
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -35,7 +35,7 @@ class Controller extends BaseController
             ]);
             return json_encode(['message'=>"successful", 'name' => $user->name, 'username'=> $user->username, 'email'=> $user->email, 'id'=> $user->id, 'points'=>0]);
         }
-        catch(ErrorException $error)
+        catch(Exception $error)
         {
             return json_encode(['message'=>$error]);
         }
@@ -43,38 +43,46 @@ class Controller extends BaseController
     
     public function set_user_type(Request $request)
     {
-        $inputs = array();
-        $inputs = file_get_contents('php://input');
-        $inputs = json_decode($inputs);
-
-        if ($inputs->user_type == 'user')
+        try
         {
-            Philanthropist::create([
-                'user_id'=>$inputs->id,
-                'contact_number'=>$inputs->contact_number,
-                'birthday'=>$inputs->birthday,
-                'sex'=>$inputs->sex,
-                'charity_category_id'=>CharityCategory::where('name', $inputs->category)->value('id'),
-            ]);
-            PhilanthropistPoint::create([
-                'user_id'=>$inputs->id,
-                'points'=>0
-            ]);
-            User::findOrFail($inputs->id)->update(['role_id'=>Role::where('name','user')->value('id')]);
-        }
-        elseif ($inputs->user_type == 'charity')
-        {
-            Charity::create([
-                'user_id'=>$inputs->id,
-                'organization'=>$inputs->organization,
-                'contact_number'=>$inputs->contact_number,
-                'account_number'=>$inputs->account_number,
-                'charity_category_id'=>CharityCategory::where('name', $inputs->category)->value('id')
-            ]);
-            User::findOrFail($inputs->id)->update(['role_id'=>Role::where('name','charity')->value('id')]);
-        }
+            $inputs = array();
+            $inputs = file_get_contents('php://input');
+            $inputs = json_decode($inputs);
 
-        return json_encode(['message'=>'successful']);
+            if ($inputs->user_type == 'user')
+            {
+                $philanthropist = Philanthropist::create([
+                    'user_id'=>$inputs->id,
+                    'contact_number'=>$inputs->contact_number,
+                    'birthday'=>$inputs->birthday,
+                    'sex'=>$inputs->sex,
+                    'charity_category_id'=>CharityCategory::where('name', $inputs->category)->value('id'),
+                ]);
+                PhilanthropistPoint::create([
+                    'philanthropist_id'=>$philanthropist->id,
+                    'points'=>0
+                ]);
+                User::findOrFail($inputs->id)->update(['role_id'=>Role::where('name','user')->value('id')]);
+            }
+            elseif ($inputs->user_type == 'charity')
+            {
+                Charity::create([
+                    'user_id'=>$inputs->id,
+                    'organization'=>$inputs->organization,
+                    'contact_number'=>$inputs->contact_number,
+                    'account_number'=>$inputs->account_number,
+                    'address'=>$inputs->address,
+                    'charity_category_id'=>CharityCategory::where('name', $inputs->category)->value('id')
+                ]);
+                User::findOrFail($inputs->id)->update(['role_id'=>Role::where('name','charity')->value('id')]);
+            }
+
+            return json_encode(['message'=>'successful']);
+        }
+        catch(Exception $error)
+        {
+            return json_encode(['message'=>$error]);
+        }
     }
 
     public function login_user()
@@ -92,27 +100,20 @@ class Controller extends BaseController
                 {
                     $current_user = User::findOrFail($user->id);
                     $user_type='';
-                    try
-                    {
-                        $user_type = $current_user->role->name;
-                    }
-                    catch(ErrorException $e)
-                    {
-
-                    }
                     $points = 0;
+                    if ($current_user->role==null)
+                    {
+                        $user_type='';
+                    }
                     if ($user_type == 'user')
                     {
                         $points = $current_user->points->points;
                     }
                     return json_encode(['message'=>"successful", 'name' => $current_user->name, 'username'=> $current_user->username, 'email'=> $current_user->email, 'id'=> $current_user->id, 'user_type'=>$user_type, 'points'=>$points]);
                 }
-                else
-                {
-                    return json_encode(['message'=>"invalid"]);
-                }
+                return json_encode(['message'=>"invalid"]);
             }
-        } catch (ErrorException $error) {
+        } catch (Exception $error) {
             return json_encode(['message'=>$error]);
         }
     }
