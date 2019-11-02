715403273f8643ed1c622f7fc74daa986a099bf2
tiiimm <51147200+tiiimm@users.noreply.github.com>
2019-11-02 18:41:19 +0800

    added apis

--- routes/api.php
+++ routes/api.php
@@ -24,12 +24,28 @@ Route::post('/register_user', "Controller@register_user");
 Route::post('/login_user', "Controller@login_user");
 Route::post('/set_user_type', "Controller@set_user_type");
 Route::post('/reward_user', "Controller@reward_user");
+Route::post('/update_profile', "Controller@update_profile");
+
 Route::post('/add_achievement', "Controller@add_achievement");
 Route::post('/add_role', "Controller@add_role");
 Route::post('/add_charity_category', "Controller@add_charity_category");
 Route::post('/add_user', "Controller@add_user");
 
-Route::get('/charities', "Controller@charities");
+Route::post('/update_achievement', "Controller@update_achievement");
+Route::post('/update_role', "Controller@update_role");
+Route::post('/update_charity_category', "Controller@update_charity_category");
+Route::post('/update_user', "Controller@update_user");
+
+Route::post('/delete_achievement', "Controller@delete_achievement");
+Route::post('/delete_role', "Controller@delete_role");
+Route::post('/delete_charity_category', "Controller@delete_charity_category");
+Route::post('/delete_user', "Controller@delete_user");
+
+Route::get('/get_achievements', "Controller@get_achievements");
+Route::get('/get_charities', "Controller@get_charities");
+Route::get('/get_roles', "Controller@get_roles");
+Route::get('/get_charity_categories', "Controller@get_charity_categories");
+Route::get('/get_users', "Controller@get_users");
 Route::get('/seed', function () {
     Role::create([
         'name' => 'admin'
