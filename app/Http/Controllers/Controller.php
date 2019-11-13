dfa8c7c97e26a091a9d078872a80b19d7ce450f0
tiiimm <51147200+tiiimm@users.noreply.github.com>
2019-11-13 14:37:18 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -855,11 +855,11 @@ class Controller extends BaseController
 
     public function get_top_charities()
     {
-        return Charity::orderBy('points')->paginate(3);
+        return Charity::join('charity_points', 'charity_points.charity_id', 'charities.id')->orderBy('points')->paginate(3);
     }
 
     public function get_latest_events()
     {
-        CharityEvent::latest()->paginate(4);
+        return CharityEvent::latest()->paginate(4);
     }
 }
